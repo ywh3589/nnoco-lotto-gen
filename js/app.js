9f3f6507b2e28e322ee3bf893971b2b8eccc5c31
Junyoung <junyoung.plum@gmail.com>
2016-06-18 17:14:00 +0900

    주석 수정(hotfix1)

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ numbers.push(number);
 }
 return numbers;
 }
-// 배열 안에 number가 포함되어 있는지 확인
+//= 배열 안에 number가 포함되어 있는지 확인 =//
 function contains(numbers, number) {
 for(var i=0; i<numbers.length; i++) {
 if(numbers[i] == number) return true;
